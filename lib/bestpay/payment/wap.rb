527c13e34de6d8ff8b27e1d45f2f0b7986bd0368
H.J.LeoChen <hjleochen@hotmail.com>
2015-09-15 10:51:00 +0800

    direct pay support

--- lib/bestpay/payment/wap.rb
+++ lib/bestpay/payment/wap.rb
@@ -3,16 +3,20 @@ module Bestpay
     #Wap支付
     module Wap
       GATEWAY_HOST = 'https://wappaywg.bestpay.com.cn/payWap.do'
+      DIRECT_GATEWAY_HOST = 'https://wappaywg.bestpay.com.cn/payWapDirect.do'
       REQUIRED_PARAMS = %w{MERCHANTID ORDERSEQ ORDERREQTRANSEQ ORDERDATE ORDERAMOUNT PRODUCTAMOUNT ATTACHAMOUNT CURTYPE
                            ENCODETYPE MERCHANTURL BACKMERCHANTURL BUSICODE PRODUCTID TMNUM CUSTOMERID PRODUCTDESC}
-      #默认
-      def self.payment_url(params)
-        default_params = {
-          'CURTYPE' => "RMB",# 币种
-          'ENCODETYPE' => "1",# 加密方式
-          'BUSICODE' => "0001",# 业务类型
-          'PRODUCTID' => "08",# 业务标识
-        }
+
+      DEFAULT_OPTIONS = {
+        'CURTYPE' => "RMB",# 币种
+        'ENCODETYPE' => "1",# 加密方式
+        'BUSICODE' => "0001",# 业务类型
+        'PRODUCTID' => "08",# 业务标识
+      }
+
+      #生成支付链接。
+      def self.generate_payment_url(in_params,host)
+        params = in_params.dup
 
         #DONE proc amount unit
         #协议定制者真是...,其他所有支付·退款都是分位单位，就WAP支付这用元为单位，没带小数点还不行(或许是补救措施吧)，
@@ -23,12 +27,16 @@ module Bestpay
         params['PRODUCTAMOUNT'] = proc_amount(params['PRODUCTAMOUNT'])
         params['ATTACHAMOUNT'] = proc_amount(params['ATTACHAMOUNT'])
 
-        Payment.build_payment_url(default_params, params, GATEWAY_HOST, REQUIRED_PARAMS)
+        Payment.build_payment_url(DEFAULT_OPTIONS, params, GATEWAY_HOST, REQUIRED_PARAMS)
       end
 
-      #自定义银行交易请求
-      def self.direct_payment_url
-        raise 'TODO'
+      def self.payment_url(params)
+        generate_payment_url(params, GATEWAY_HOST)
+      end
+
+      #自定义银行交易请求, 唯一区别是： 需要传入BANKID, 无需两次输入密码。
+      def self.direct_payment_url(params)
+        generate_payment_url(params, DIRECT_GATEWAY_HOST)
       end
 
       protected
